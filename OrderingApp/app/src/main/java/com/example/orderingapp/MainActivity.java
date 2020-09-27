aca9cd31ef66d58563d23dba292af506539e8f3f
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-09-27 16:40:48 +0530

    Added plus and minus button to increase and decrease the number of orders

--- OrderingApp/app/src/main/java/com/example/orderingapp/MainActivity.java
+++ OrderingApp/app/src/main/java/com/example/orderingapp/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
-
+    int quantity = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +21,27 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 5;
         display(quantity);
-        displayPrice(quantity* 5);
+        displayPrice(quantity*5);
     }
 
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void  increment(View view)
+    {
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view)
+    {
+        quantity = quantity - 1;
+        display(quantity);
+    }
     /**
      * This method displays the given quantity value on the screen.
      */
