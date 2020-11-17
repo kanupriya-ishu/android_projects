c934c3ba9532ef4fda5a15a14d51deb7d5572dcf
Kanupriya Ishu <kanupriya2512@gmail.com>
2020-11-17 23:52:43 +0530

    Added ArrayList in NumbersActivity file

--- Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import android.util.Log;
 
+import java.util.ArrayList;
+
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -13,18 +15,18 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        //Create an array of words
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        // Create a list of words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
         Log.v("NumbersActivity", "Word at index 0: " + words[0]);
         Log.v("NumbersActivity", "Word at index 1: " + words[1]);
