f2a584bbf92fe281babc5fb79f19aab00b462501
Kanupriya Ishu <kanupriya2512@gmail.com>
2021-01-18 23:34:02 +0530

    Added getDefaultTranslation and getMiwokTranslation methods

--- Miwok/app/src/main/java/com/example/android/miwok/Word.java
+++ Miwok/app/src/main/java/com/example/android/miwok/Word.java
@@ -1,4 +1,27 @@
 package com.example.android.miwok;
 
 public class Word {
+
+    private String mDefaultTranslation;
+
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    /**
+     * Get the default translation of the word
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
 }
