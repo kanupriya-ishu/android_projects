6e29a1d9d59511bdf495b6daa245feed32412abf
kanupriya-ishu <kanupriya2512@gmail.com>
2021-02-07 22:52:39 +0530

    Update WordAdapter.java

--- TourGuide/app/src/main/java/com/example/tourguide/WordAdapter.java
+++ TourGuide/app/src/main/java/com/example/tourguide/WordAdapter.java
@@ -1,7 +1,15 @@
 package com.example.tourguide;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 
@@ -14,5 +22,36 @@ public class WordAdapter extends ArrayAdapter<Word> {
         mColorResourceId = colorResourceId;
     }
 
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View listItemView = convertView;
+
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        Word word = getItem(position);
+
+        TextView name = listItemView.findViewById(R.id.name_text_view);
+        TextView description = listItemView.findViewById(R.id.description_text_view);
+
+        name.setText(word.getName());
+        description.setText(word.getDescription());
+
+        ImageView imageView = listItemView.findViewById(R.id.image);
+
+        imageView.setVisibility(View.VISIBLE);
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
+
+        return listItemView;
+    }
 
 }
